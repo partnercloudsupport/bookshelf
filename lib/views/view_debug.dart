d7fd190c88fe1ffc9d8746ed103ce638a0b7988b
mchome <280604399@qq.com>
2018-03-19 16:14:44 +0800

    update

--- lib/views/view_debug.dart
+++ lib/views/view_debug.dart
@@ -7,36 +7,37 @@ import 'package:flutter_advanced_networkimage/transition_to_image.dart';
 class ViewDebug extends StatefulWidget {
   @override
   _ViewDebugState createState() => new _ViewDebugState();
-
 }
 
 class _ViewDebugState extends State<ViewDebug> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
-//    return new Column(
-//      children: <Widget>[
-//        new SizedBox(
-//          height: 500.0,
-//          child: new Container(
-//            child: new TransitionToImage(
-//              new AdvancedNetworkImage('https://i.nhentai.net/galleries/1175501/3.jpg', useMemoryCache: false),
-//            ),
-//          ),
-//        ),
-//        new Expanded(
-//            child: new Container(
-//              color: Colors.green,
-//            )
-//        )
-//      ],
-//    );
-    return new Scaffold(
-      appBar: new AppBar(title: const Text('debug workshop')),
-      body: new Material(
-        child: new Center(
-          child: new ColorPicker(),
+    return new Column(
+      children: <Widget>[
+        new SizedBox(
+          height: 500.0,
+          child: new Container(
+            color: Colors.white,
+            child: new TransitionToImage(
+              new AdvancedNetworkImage(
+                  'https://i.nhentai.net/galleries/1175501/4.jpg',
+                  useMemoryCache: false),
+            ),
+          ),
         ),
-      ),
+        new Expanded(
+            child: new Container(
+          color: Colors.green,
+        ))
+      ],
     );
+//    return new Scaffold(
+//      appBar: new AppBar(title: const Text('debug workshop')),
+//      body: new Material(
+//        child: new Center(
+//          child: new ColorPicker(),
+//        ),
+//      ),
+//    );
   }
 }
